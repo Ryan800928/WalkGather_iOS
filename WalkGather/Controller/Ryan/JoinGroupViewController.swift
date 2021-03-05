0c5a885886ccad0c29cf4c53691c2e4bbd8800c9
Ryan <ratedr0928@gmail.com>
2021-03-05 21:46:32 +0800

    1. 新增Jack的StoryBoard

---
+++ WalkGather/Controller/Ryan/JoinGroupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JoinGroupViewController.swift
+//  WalkGather
+//
+//  Created by Ryan on 2021/2/22.
+//
+
+import UIKit
+
+class JoinGroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
